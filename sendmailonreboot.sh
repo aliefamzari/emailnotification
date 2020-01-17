91b67b493d8751107dd5196c491ad70d5d2f8c29
aliefamzari <aliefamzari@gmail.com>
2020-01-17 15:11:59 +0800

    introduce while loop before email being sent

--- sendmailonreboot.sh
+++ sendmailonreboot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#On the event of init 6, email will be send to notify the reboot event
+# On the event of init 6, email will be send to notify the reboot event
+# Author : Alif Amzari Mohd Azamee
+# Version control : Git
+# License: MIT
+
+
 sleep 60
 # Global variables
 noipDnsName="fiftysixflex.ddns.net"
@@ -7,36 +12,49 @@ ip=`hostname -I`
 lhost=`hostname -f`
 recipient="aliefamzari@gmail.com"
 tmpPath="/tmp"
-publicIP=`nslookup $noipDnsName |grep 'Address' |grep -v 192 |awk '{print $2}'`
+ddns=$(dig +short $noipDnsName)
 uptimeSince=`uptime -s`
 currentDate=`date +"%Y-%m-%d %H:%M:%S"`
+currentIp=$(curl -s /dev/null ifconfig.co 2>&1)
 
 # Services detail
 apache=$(systemctl status apache2 |grep Active)
 mariadb=$(systemctl status mariadb |grep Active)
 mdadmdetail=$(sudo mdadm --detail /dev/md0)
 
-# Populating information into email.txt 
-echo "Server status report: Reason 'Unexpected REBOOT'" > $tmpPath/email.txt
-echo "Localhost: $lhost" >> $tmpPath/email.txt
-echo "Current Date: $currentDate" >> $tmpPath/email.txt
-echo "Uptime Since: $uptimeSince" >> $tmpPath/email.txt
-echo "Local IP: $ip" >> $tmpPath/email.txt
-if ping -c 1 -W 1 "$noipDnsName" > /dev/null ; then
-	echo -e "DNS status: $noipDnsName is alive." >> $tmpPath/email.txt
-	echo -e "Public IP: $publicIP" >> $tmpPath/email.txt
-	else
-	echo -e "DNS Status: $noipDnsName is dead." >> $tmpPath/email.txt
-fi
-echo -e "Apache status:$apache"  >> $tmpPath/email.txt
-echo -e "Mariadb status:$mariadb" >> $tmpPath/email.txt
-echo -e "RAID status: $mdadmdetail" >> $tmpPath/email.txt
-echo >> $tmpPath/email.txt
-echo "####END OF SERVER STATUS REPORT####" >> $tmpPath/email.txt
-# Finished populating information
+# Loop 5 times if no-ip and currentIP is not match.  Code exit after 5 times. 
+n=1 
+while [ $n -le 5 ]; do
+    ddns=$(dig +short $noipDnsName)
+    if [ ${currentIp} != $ddns ]; then
+    echo 'Retrying..'
+    n=$(( n+1 ))
+    sleep 30
+    else
+    # Populating content into payload
+        echo "Server status report: Reason 'Unexpected REBOOT'" > $tmpPath/email.txt
+        echo "Localhost: $lhost" >> $tmpPath/email.txt
+        echo "Current Date: $currentDate" >> $tmpPath/email.txt
+        echo "Uptime Since: $uptimeSince" >> $tmpPath/email.txt
+        echo "Local IP: $ip" >> $tmpPath/email.txt
+        if ping -c 1 -W 1 "$noipDnsName" > /dev/null ; then
+        	echo -e "DNS status: $noipDnsName is alive." >> $tmpPath/email.txt
+        	echo -e "Public IP: $ddns" >> $tmpPath/email.txt
+        	else
+        	echo -e "DNS Status: $noipDnsName is dead." >> $tmpPath/email.txt
+        fi
+        echo -e "Apache status:$apache"  >> $tmpPath/email.txt
+        echo -e "Mariadb status:$mariadb" >> $tmpPath/email.txt
+        echo -e "RAID status: $mdadmdetail" >> $tmpPath/email.txt
+        echo >> $tmpPath/email.txt
+        echo "####END OF SERVER STATUS REPORT####" >> $tmpPath/email.txt
+       
 
-# Sending message to target recipient 
-mail -s "$lhost at $noipDnsName online" $recipient < $tmpPath/email.txt
+        # Sending message to target recipient 
+        mail -s "$lhost at $noipDnsName online" $recipient < $tmpPath/email.txt
+        # Delete /$tmpPath/email.txt if file exist
+        test -e $tmpPath/email.txt && rm $tmpPath/email.txt
+        exit 0
+    fi
+done
 
-#Delete /$tmpPath/email.txt if file exist
-test -e $tmpPath/email.txt && rm -rf $tmpPath/email.txt
